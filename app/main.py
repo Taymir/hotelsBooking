f853d5a8ee26563bb28949adb3beca7726b95271
Tim <taymir@gmail.com>
2023-07-03 18:09:14 +0300

    Реализована админка

--- app/main.py
+++ app/main.py
@@ -11,7 +11,13 @@ from fastapi_cache.decorator import cache
 
 from redis import asyncio as aioredis
 
+from sqladmin import Admin, ModelView
+
+from app.admin.auth import authentication_backend
+from app.admin.views import UsersAdmin, BookingsAdmin, HotelsAdmin, RoomsAdmin
 from app.config import settings
+from app.database import engine
+from app.users.models import Users
 from app.users.router import router as router_users
 from app.bookings.router import router as router_bookings
 from app.hotels.router import router as router_hotels
@@ -48,3 +54,10 @@ app.add_middleware(
 async def startup():
     redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}")
     FastAPICache.init(RedisBackend(redis), prefix="cache")
+
+admin = Admin(app, engine, authentication_backend=authentication_backend)
+
+admin.add_view(UsersAdmin)
+admin.add_view(BookingsAdmin)
+admin.add_view(HotelsAdmin)
+admin.add_view(RoomsAdmin)
