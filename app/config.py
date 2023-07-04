a28923bbec50dc4b751a4894abb80058cb9d9e70
Tim <taymir@gmail.com>
2023-07-04 18:20:57 +0300

    Добавлены фикстуры для тестов

--- app/config.py
+++ app/config.py
@@ -1,12 +1,22 @@
+from typing import Literal
+
 from pydantic import BaseSettings, root_validator
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
 
     JWT_SECRET: str
     JWT_HASH: str
@@ -23,6 +33,13 @@ class Settings(BaseSettings):
     def get_database_url(cls, v):
         v['DATABASE_URL'] = \
             f"postgresql+asyncpg://{v['DB_USER']}:{v['DB_PASS']}@{v['DB_HOST']}:{v['DB_PORT']}/{v['DB_NAME']}"
+        return v
+
+
+    @root_validator
+    def get_test_database_url(cls, v):
+        v['TEST_DATABASE_URL'] = \
+            f"postgresql+asyncpg://{v['TEST_DB_USER']}:{v['TEST_DB_PASS']}@{v['TEST_DB_HOST']}:{v['TEST_DB_PORT']}/{v['TEST_DB_NAME']}"
         return v
 
     class Config:
