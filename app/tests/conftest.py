c3fb2f634306ceaf1986109ff71526b96c9afc2c
Tim <taymir@gmail.com>
2023-07-05 16:47:18 +0300

    Написаны тесты dao для bookings, users

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -4,7 +4,10 @@ from datetime import datetime
 
 import pytest
 from sqlalchemy import insert
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
+from app.main import app as fastapi_app
 from app.config import settings
 from app.database import Base, async_session_maker, engine
 
@@ -55,3 +58,15 @@ def event_loop (request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope="function")
+async def ac():
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        yield ac
+
+
+@pytest.fixture(scope="function")
+async def session():
+    async with async_session_maker() as session:
+        yield session
--- app/users/router.py
+++ app/users/router.py
@@ -17,7 +17,7 @@ router = APIRouter(
 async def register_user(user_data: SUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
-        raise UserAlreadyExistsException()
+        raise UserAlreadyExistsException
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
 
@@ -26,7 +26,7 @@ async def register_user(user_data: SUserAuth):
 async def login_user(response: Response, user_data: SUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
-        raise IncorrectCredentialsException()
+        raise IncorrectCredentialsException
     access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return access_token
